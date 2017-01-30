0da47aa5a937d7c12dbecea4e4b3e010836561f2
Philipp Marek <philipp.marek@linbit.com>
2017-01-30 10:13:55 +0100

    Convert idr_for_each_entry(), too.

--- conversion-scripts/0100-idr_for_each_entry-type.pl
+++ conversion-scripts/0100-idr_for_each_entry-type.pl
@@ -3,21 +3,36 @@
 
 our %types;
 sub BEGIN {
-%types = qw(   	peer_device drbd_peer_device
-				r			drbd_request
-				e			lc_element
-				req			drbd_request
-				drbd_path	drbd_path
-				);
+%types = qw(    peer_device drbd_peer_device
+                device      drbd_device
+                r           drbd_request
+                e           lc_element
+                req         drbd_request
+                drbd_path   drbd_path
+                );
 };
 
-s{ 
+
+# - idr_for_each_entry(&resource->devices, device, vnr)
+# + idr_for_each_entry(struct drbd_device *, &resource->devices, device, vnr)
+s{
+	( \s idr_for_each_entry
+		\( [^,]+, )
+	((\w+),)
+}{
+	my $t = $types{$3};
+	$t ?  $1 . "struct $t *, $2"  : "$1$2";
+}xe;
+
+# - idr_for_each_entry(&resource->devices, device, vnr)
+# + idr_for_each_entry(struct drbd_device *, &resource->devices, device, vnr)
+s{
 	( \s (?: list_for_each_entry ( _continue | _safe | _reverse | _rcu )* 
 		| list_next_entry ) 
 		\( )
-	((\w+),) 
+	((\w+),)
 }{
 	my $t = $types{$4};
-	$t ?  $1 . "struct $t, " . $3 : "$1$3";
+	$t ?  $1 . "struct $t, $3" : "$1$3";
 }xe;
 
