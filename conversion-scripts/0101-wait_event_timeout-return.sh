80ec93d04439eae6ecbd6ef6e14e29bb77a0ed82
Philipp Marek <philipp.marek@linbit.com>
2017-01-30 10:12:56 +0100

    Don't need a bash inbetween.

--- conversion-scripts/0101-wait_event_timeout-return.sh
+++ conversion-scripts/0101-wait_event_timeout-return.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/usr/bin/perl -pi.bak
 
 # - 	t = wait_event_timeout(device->misc_wait,
 # +    wait_event_timeout(t, device->misc_wait,
 
-perl -i.bak -pe '
 s/^(\s*(\w+)\s*=\s*wait_event_timeout\()/$1$2, /;
-' "$@"
