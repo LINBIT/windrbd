7f6a73741bbce218ab2a02b3ba297d3582deae2f
Philipp Marek <philipp.marek@linbit.com>
2017-01-31 15:24:41 +0100

    Remove some old trash from ms-cl.cmd, to make it silent.

--- ms-cl.cmd
+++ ms-cl.cmd
@@ -1,14 +1,3 @@
-
-rem set VC_BASE="%1"
-
-call "../../../installs/ewdk/BuildEnv/SetupBuildEnv.cmd" amd64
-
-date /t
-time /t
-
-rem VC := env PATH="$(PATH);$(VC_BASE)/../MSBuild/14.0/Bin" "$(VC_BASE)/VC/bin/cl.exe" 
-rem
-
-rem set VC_CL_PATH=z:\src\drbd-9\drbd\..\..\..\installs\ewdk\Program Files\Microsoft Visual Studio 14.0\..\MSBuild\14.0\Bin\x86_64\cl.exe
-rem "%VC_CL_PATH%" %*
-cl.exe %*
+@echo off
+@call "../../../installs/ewdk/BuildEnv/SetupBuildEnv.cmd" amd64
+@cl.exe %*
