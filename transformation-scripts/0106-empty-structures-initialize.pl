175f4ce0a042d7cc9c3c3317bf0b870a332d3230
Philipp Marek <philipp.marek@linbit.com>
2017-02-08 11:00:45 +0100

    Fix quoting on the RE replacement result.

--- transformation-scripts/0106-empty-structures-initialize.pl
+++ transformation-scripts/0106-empty-structures-initialize.pl
@@ -4,5 +4,5 @@
 # -   union drbd_state mask = {}, val = {};
 # +   union drbd_state mask = { 0, };
 # +   union drbd_state val = { 0, };
-s( (= \s* \{) \s* \} )( "$1 0 }" )gx;
+s( (= \s* \{) \s* \} )( "$1 0 }" )gxe;
 
