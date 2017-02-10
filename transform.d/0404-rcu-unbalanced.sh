dfaf06cf0d1aeb0cc10d978165dc5fb9a41e68b8
Philipp Marek <philipp.marek@linbit.com>
2017-02-10 10:58:11 +0100

    Don't print tmp filenames during "make".

--- transform.d/0404-rcu-unbalanced.sh
+++ transform.d/0404-rcu-unbalanced.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # you have to make SURE that this is executed after the balanced replace
-echo $1 | grep drbd_state.c || exit 0 # maybe add more later
+echo $1 | grep -q drbd_state.c || exit 0 # maybe add more later
 sed -ie 's/rcu_read_unlock();/rcu_read_unlock(resource->wrcu_flags);/' $1
 sed -ie 's/^\srcu_read_lock();/resource->wrcu_flags = rcu_read_lock();/' $1
