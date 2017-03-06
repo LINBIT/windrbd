46457c1f070a54f8058d5432ddc59a892ebce246
Philipp Marek <philipp.marek@linbit.com>
2017-03-06 15:03:21 +0100

    Avoid duplicated "BUILD_BUG_ON()" definitions, seen for "drbd_nl.E".

--- win4lin/linux/kernel.h
+++ win4lin/linux/kernel.h
@@ -1,7 +1,9 @@
 ﻿#ifndef __KERNEL_H__
 #define __KERNEL_H__
 
+#ifndef BUILD_BUG_ON
 #define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
+#endif
 
 #define ULLONG_MAX	(~0ULL)
 
