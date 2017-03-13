0cfa6a5987dd054b526a73138e83e91b45064ba9
Philipp Marek <philipp.marek@linbit.com>
2017-03-13 12:24:05 +0100

    Simplify the preproc transform - no need to call a bash to run perl.

--- transform.d/001-remove-other-preproc.pl
+++ transform.d/001-remove-other-preproc.pl
@@ -1,19 +1,17 @@
-#!/bin/bash
-
-
-perl -i.bak -pe '
+#!/usr/bin/perl -pi.bak
 
 # BVD => ->
 s/\s+BVD\s+/->/g;
 
-# In 
-#   #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt  
+# In
+#   #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 # coccinelle doesn´t like KBUILD_MODNAME
 s/^.define pr_fmt.*\sKBUILD_MODNAME.*/#define pr_fmt(fmt) ":" fmt/;
+
+s/\s__printf\([ 0-9,]+\)\s/ /g;
 
 # collapse lines
 # s/\\\r?\n$/ /g; ## doesn´t help
 
-' "$@"
 
 
