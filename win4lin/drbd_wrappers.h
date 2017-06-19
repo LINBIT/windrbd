f2988e6fa64471de2954438e0298f011e3cf5049
Philipp Marek <philipp.marek@linbit.com>
2017-06-19 07:23:58 +0200

    No need to negate the ?: expression, just switch the results.

--- win4lin/drbd_wrappers.h
+++ win4lin/drbd_wrappers.h
@@ -1053,7 +1053,7 @@ static inline u32 prandom_u32(void)
 
 #ifndef list_first_entry_or_null
 #define list_first_entry_or_null(ptr, type, member) \
-	(!list_empty(ptr) ? list_first_entry(ptr, type, member) : NULL)
+	(list_empty(ptr) ? NULL : list_first_entry(ptr, type, member))
 #endif
 
 #ifndef COMPAT_HAVE_IDR_ALLOC
