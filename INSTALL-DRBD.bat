e79df68dbef54862db042a4811de9ee6658cc172
Johannes Thoma <johannes@johannesthoma.com>
2017-08-15 21:45:14 +0200

    Cleaned up INSTALL-DRBD.bat (user level install step) a little.

--- INSTALL-DRBD.bat
+++ INSTALL-DRBD.bat
@@ -1,5 +1,8 @@
-@echo OFF
-
+rem This script currently just copies neccessary stuff to 
+rem the Windows temp (%Temp%/drbd) directory. I am not sure
+rem if we really need this step.
+rem
+rem @echo OFF
 
 rem change drive, too...
 cd /D "%0\.."
@@ -25,16 +28,15 @@ mkdir "%TMPCPYDIR%\var\lib\drbd"
 mkdir "%TMPCPYDIR%\var\run"
 mkdir "%TMPCPYDIR%\var\run\drbd"
 
-rem /COPYALL doesn't work, not enough rights
-rem robocopy /MIR /COPY:DT "src\git-complete\build\drbd" "%TMPCPYDIR%"
 copy "%0\..\..\drbd-utils\user\v9\*.exe" "%TMPCPYDIR%\bin"
-rem copy "c:\cygwin64\bin\cygwin1.dll" "%TMPCPYDIR%\bin"
+rem You need to change this to the location of cygwin on your system.
 copy "c:\cygwin64\bin\cygwin1.dll" "%TMPCPYDIR%\bin"
 
 copy "..\converted-sources-working-copy\drbd\drbd.sys" "%TMPCPYDIR%\setup"
 copy "..\converted-sources-working-copy\drbd\drbd.cat" "%TMPCPYDIR%\setup"
 copy "..\converted-sources-working-copy\drbd\drbd.inf" "%TMPCPYDIR%\setup"
 copy "..\converted-sources-working-copy\crypto\linbit.cer"               "%TMPCPYDIR%\setup"
+rem You need to change this to the location of Ewdk on your system.
 copy "c:\Ewdk\Program Files\Windows Kits\10\bin\x64\certmgr.exe"     "%TMPCPYDIR%\setup"
 
 copy ".\etc\drbd.conf" "%TMPCPYDIR%\etc"
@@ -44,47 +46,12 @@ copy ".\etc\drbd.d\*.conf" "%TMPCPYDIR%\etc\drbd.d"
 copy ".\INSTALL-DRBD-admin.bat" "%TMPCPYDIR%"
 
 rem RUNAS always asks for a password, unless credentials have already been saved via /SAVECRED
-rem   runas /user:administrator      "%TMPCPYDIR%\INSTALL-DRBD-admin.bat"
-rem so we're using PSEXEC instead
-rem   https://technet.microsoft.com/en-us/sysinternals/bb897553
-rem
-rem windows-install\pstools\psexec  \\%computername% -i -h cmd.exe /c "%TMPCPYDIR%\INSTALL-DRBD-admin.bat"
+runas /user:administrator "%TMPCPYDIR%\INSTALL-DRBD-admin.bat"
 
-
-if _%RUN_INST% == _yes goto inst
-
-echo "--------------------------------------------------------------------"
-echo "Please run "
-echo      %TMPCPYDIR%\INSTALL-DRBD-admin.bat
-echo "as an adminstrator
-rem start explorer "%TMPCPYDIR%"
-echo "--------------------------------------------------------------------"
-
-rem We need some post-install hooks, don't do them for now.
-goto exit
-
-:inst
-rem runas /user:administrator      "%0\..\INSTALL-DRBD-admin.bat"
-"%TMPCPYDIR%\INSTALL-DRBD-admin.bat"
-pause
-
-
-
-
-
-
-
-
-
-xcopy /E /F /H /Y "src\git-complete\build\drbd\x86" "c:\program files (x86)\drbd\"
-xcopy /E /F /H /Y "src\git-complete\build\drbd\x86" "c:\program files (x86)\drbd\"
-
-xcopy /E /F /H /Y ".\src\git-complete\build\drbd\x86\Win7Debug" "c:\program files (x86)\drbd\inf\"
-runas /user:administrator      "%0\..\INSTALL-DRBD-admin.bat"
-
-
-rem %SystemRoot%\System32\InfDefaultInstall.exe ".\src\git-complete\build\drbd\x86\Win7Debug\bin\drbd.inf"
-
-pause
-
-:exit
+echo "Type"
+echo "sc drbd start"
+echo "to load the driver"
+echo
+echo "Note that you must disable Windows driver signature verification"
+echo "on boot (for Windows 7, press F8 on boot and select Disable driver"
+echo "signature verification (or so))"
