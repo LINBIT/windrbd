47750148c691f3df4b71590471522936333cdeaf
Johannes Thoma <johannes@johannesthoma.com>
2017-10-21 15:05:21 +0200

    Started working on ioctl's (DeviceControls)

--- win4lin/src/windrbd_device.c
+++ win4lin/src/windrbd_device.c
@@ -1,5 +1,6 @@
 #include <wdm.h>
 #include <ntddk.h>
+#include <ntdddisk.h>
 
 #include "drbd_windows.h"
 #include "windrbd_device.h"
@@ -16,10 +17,30 @@ static NTSTATUS windrbd_not_implemented(struct _DEVICE_OBJECT *device, struct _I
 //	return STATUS_NOT_IMPLEMENTED;
 }
 
+static NTSTATUS windrbd_device_control(struct _DEVICE_OBJECT *device, struct _IRP *irp)
+{
+	struct _IO_STACK_LOCATION *s = IoGetCurrentIrpStackLocation(irp);
+
+	printk(KERN_DEBUG "IoCtl: device: %p irp: %p s: %p s->MajorFunction: %x s->MinorFunction: %x s->Parameters.DeviceIoControl.IoControlCode: %x\n", device, irp, s, s->MajorFunction, s->MinorFunction, s->Parameters.DeviceIoControl.IoControlCode);
+	switch (s->Parameters.DeviceIoControl.IoControlCode) {
+	case IOCTL_DISK_GET_DRIVE_GEOMETRY:
+		printk(KERN_DEBUG "get drive geometry.\n");
+		break;
+	default: ;
+	}
+
+	irp->IoStatus.Status = STATUS_SUCCESS;
+        IoCompleteRequest(irp, IO_NO_INCREMENT);
+        return STATUS_SUCCESS;
+}
+
 void windrbd_set_major_functions(struct _DRIVER_OBJECT *obj)
 {
 	int i;
 
 	for (i=0; i<IRP_MJ_MAXIMUM_FUNCTION; i++)
 		obj->MajorFunction[i] = windrbd_not_implemented;
+
+	obj->MajorFunction[IRP_MJ_DEVICE_CONTROL] = windrbd_device_control;
+
 }
