f365d541873084398ef12b6706f7d2c6c5804f42
Johannes Thoma <johannes@johannesthoma.com>
2018-02-22 18:36:18 +0100

    Handle case where cygwin is already installed on target
    drbdadm should work from bash as well as from cmd shell.

--- install-windrbd.cmd
+++ install-windrbd.cmd
@@ -1,6 +1,22 @@
 copy *.exe c:\windows\System32
-copy *.dll c:\windows\System32
 
+if exist c:\cygwin\NUL goto found_cygwin1
+if exist c:\cygwin64\NUL goto found_cygwin2
+
+copy *.dll c:\windows\System32
+goto next
+
+:found_cygwin1
+copy c:\cygwin\bin\cygwin1.dll c:\windows\System32
+copy c:\cygwin\bin\cygbz2-1.dll c:\windows\System32
+goto next
+
+:found_cygwin2
+copy c:\cygwin64\bin\cygwin1.dll c:\windows\System32
+copy c:\cygwin64\bin\cygbz2-1.dll c:\windows\System32
+goto next
+
+:next
 if exist c:\windrbd\NUL goto keep_settings
 
 mkdir c:\windrbd
