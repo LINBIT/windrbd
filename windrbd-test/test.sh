14fbe48e56cca63e3615701a5c808420f258c380
Johannes Thoma <johannes@johannesthoma.com>
2018-04-25 19:05:00 +0200

    TODO (and minor test) update

--- windrbd-test/test.sh
+++ windrbd-test/test.sh
@@ -18,7 +18,8 @@ drbdadm down $RES
 
 drbdadm up $RES
 drbdadm primary $RES
-# echo -e 'j\nj\nj\nj\nj\n' | format.com ${DRIVE_LETTER}:
+echo -e 'j\nj\nj\nj\nj\n' | format.com ${DRIVE_LETTER}:
+sleep 5
 cp /dev/urandom /cygdrive/${DRIVE_LETTER}/data
 drbdadm secondary $RES
 drbdadm down $RES
