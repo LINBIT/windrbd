07ede11b8b92a285e1bd87093d77e81f9bc8daa7
Johannes Thoma <johannes@johannesthoma.com>
2018-06-22 22:26:29 +0200

    Returing invalid request code for non-implemented ioctl's
    Not a good idea: access to a NTFS file system fails.

--- win4lin/src/windrbd_device.c
+++ win4lin/src/windrbd_device.c
@@ -304,12 +304,12 @@ static NTSTATUS windrbd_device_control(struct _DEVICE_OBJECT *device, struct _IR
 		}
 
 		printk("IOCTL_STORAGE_QUERY_PROPERTY: PropertyId: %d QueryType: %d\n", query->PropertyId, query->QueryType);
-		status = STATUS_NOT_IMPLEMENTED;
+		status = STATUS_INVALID_DEVICE_REQUEST;
 		break;
 
 	default: 
 		printk(KERN_DEBUG "DRBD IoCtl request not implemented: IoControlCode: 0x%x\n", s->Parameters.DeviceIoControl.IoControlCode);
-		status = STATUS_NOT_IMPLEMENTED;
+		status = STATUS_INVALID_DEVICE_REQUEST;
 	}
 
 	irp->IoStatus.Status = status;
