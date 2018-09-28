e0d2f2034bbe2bc1403225f5205dc61080367ce7
Johannes Thoma <johannes@johannesthoma.com>
2018-09-28 21:22:28 +0200

    Revert "Make it build on our Windows 10 physical machine"
    This reverts commit 82bde46e89323bc7c69ed41d67322e13d5f6d225.

--- ms-cl.cmd
+++ ms-cl.cmd
@@ -1,4 +1,3 @@
-set EWDK_BASE=d:
-call %EWDK_BASE%\BuildEnv\SetupBuildEnv.cmd amd64
-set PATH=%PATH%;"D:\Program Files\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.14.26428\bin\Hostx64\x64"
-cl.exe %*
+@echo off
+@call %EWDK_BASE%/BuildEnv/SetupBuildEnv.cmd amd64
+@cl.exe %*
