71272a561cf106a604494d5cd0f86368aff6db23
Johannes Thoma <johannes@johannesthoma.com>
2018-10-09 19:20:31 +0200

    Do not start user mode helpers on boottime.

--- inno-setup/install-windrbd.cmd
+++ inno-setup/install-windrbd.cmd
@@ -23,7 +23,7 @@ copy windrbd.sys c:\windows\sysnative\drivers
 copy windrbd*.exe c:\windrbd\usr\sbin
 copy drbd*.exe c:\windrbd\usr\sbin
 
-cygrunsrv.exe -I windrbdlog -p /cygdrive/c/windrbd/usr/sbin/windrbd.exe -a log-server -1 /cygdrive/c/windrbd/windrbd-kernel.log -2 /cygdrive/c/windrbd/windrbd-kernel.log
-cygrunsrv.exe -I windrbdumhelper -p /cygdrive/c/windrbd/usr/sbin/windrbd.exe -auser-mode-helper-daemon -1 /cygdrive/c/windrbd/windrbd-umhelper.log -2 /cygdrive/c/windrbd/windrbd-umhelper.log
+cygrunsrv.exe -I windrbdlog -p /cygdrive/c/windrbd/usr/sbin/windrbd.exe -a log-server -1 /cygdrive/c/windrbd/windrbd-kernel.log -2 /cygdrive/c/windrbd/windrbd-kernel.log -t manual
+cygrunsrv.exe -I windrbdumhelper -p /cygdrive/c/windrbd/usr/sbin/windrbd.exe -auser-mode-helper-daemon -1 /cygdrive/c/windrbd/windrbd-umhelper.log -2 /cygdrive/c/windrbd/windrbd-umhelper.log -t manual
 
 start /wait InfDefaultInstall ".\windrbd.inf"
