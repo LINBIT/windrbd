a41eeace025f8609bf827af8643ebe24b2d3b392
Johannes Thoma <johannes@johannesthoma.com>
2018-10-18 18:40:14 +0200

    Can build signed packages on Windows 10 machine (again)

--- windrbd-test/iptables-drop-dont-drop-loop.sh
+++ windrbd-test/iptables-drop-dont-drop-loop.sh
@@ -4,8 +4,10 @@ while true
 do 
 	i=$[ $i+1 ]
 	echo "network failure $i"
+	echo "about to drop packets"
 	./iptables-drop.sh
 	sleep 20
+	echo "about to not drop packets"
 	./iptables-dont-drop.sh
 	sleep 20
 done
