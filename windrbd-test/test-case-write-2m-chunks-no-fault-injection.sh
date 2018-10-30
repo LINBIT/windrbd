15be03c9e0a09ac4c1c83a89c822163d15d5a09a
Johannes Thoma <johannes@johannesthoma.com>
2018-10-30 19:07:35 +0100

    Test that writes raw device with 2m requests.
    Full test case, no need for drbdadm up, ....

---
+++ windrbd-test/test-case-write-2m-chunks-no-fault-injection.sh
@@ -0,0 +1,16 @@
+# this should not BSOD
+# it is normal that the test shows I/O errors since we fault inject here
+
+res=${RES:-w0}
+export LOOP_CNT=${LOOP_CNT:-3}
+megs=${REQ_SIZE:-2}
+size=${SIZE:-49}
+drive=${DRIVE:-'k:'}
+
+drbdadm down $res
+drbdadm up $res
+drbdadm primary $res --force
+# windrbd inject-faults 100 backing-completion $drive
+loop.sh 'rw test' 0 write-disk-one-meg.sh $drive $[ $size*1024*1024 ] $[ $megs*1024*1024 ] rw
+drbdadm down $res
+# also fault injection disabled here .. it is per backing device which does not exist any more.
