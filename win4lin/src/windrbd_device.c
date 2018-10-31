f6c551f7bb2e0f8a355bcb79f03ae10d37ff4d2e
Johannes Thoma <johannes@johannesthoma.com>
2018-10-31 17:25:19 +0100

    Log cleanup: Not printing root device requests.

--- win4lin/src/windrbd_device.c
+++ win4lin/src/windrbd_device.c
@@ -45,7 +45,7 @@
 static NTSTATUS windrbd_not_implemented(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 {
 	if (device == mvolRootDeviceObject) {
-		printk(KERN_DEBUG "Root device request.\n");
+		dbg(KERN_DEBUG "Root device request.\n");
 
 		irp->IoStatus.Status = STATUS_SUCCESS;
 	        IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -556,7 +556,7 @@ static NTSTATUS windrbd_device_control(struct _DEVICE_OBJECT *device, struct _IR
 static NTSTATUS windrbd_create(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 {
 	if (device == mvolRootDeviceObject) {
-		printk(KERN_DEBUG "Root device request.\n");
+		dbg(KERN_DEBUG "Root device request.\n");
 
 		irp->IoStatus.Status = STATUS_SUCCESS;
 	        IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -609,7 +609,7 @@ static NTSTATUS windrbd_create(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 static NTSTATUS windrbd_close(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 {
 	if (device == mvolRootDeviceObject) {
-		printk(KERN_DEBUG "Root device request.\n");
+		dbg(KERN_DEBUG "Root device request.\n");
 
 		irp->IoStatus.Status = STATUS_SUCCESS;
 	        IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -657,7 +657,7 @@ static NTSTATUS windrbd_close(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 static NTSTATUS windrbd_cleanup(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 {
 	if (device == mvolRootDeviceObject) {
-		printk(KERN_DEBUG "Root device request.\n");
+		dbg(KERN_DEBUG "Root device request.\n");
 
 		irp->IoStatus.Status = STATUS_SUCCESS;
 	        IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -938,7 +938,7 @@ static NTSTATUS make_drbd_requests(struct _IRP *irp, struct block_device *dev)
 static NTSTATUS windrbd_io(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 {
 	if (device == mvolRootDeviceObject) {
-		printk(KERN_DEBUG "Root device request.\n");
+		dbg(KERN_DEBUG "Root device request.\n");
 
 		irp->IoStatus.Status = STATUS_SUCCESS;
 	        IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -992,7 +992,7 @@ exit:
 static NTSTATUS windrbd_shutdown(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 {
 	if (device == mvolRootDeviceObject) {
-		printk(KERN_DEBUG "Root device request.\n");
+		dbg(KERN_DEBUG "Root device request.\n");
 
 		irp->IoStatus.Status = STATUS_SUCCESS;
 	        IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -1042,7 +1042,7 @@ static void windrbd_bio_flush_finished(struct bio * bio, int error)
 static NTSTATUS windrbd_flush(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 {
 	if (device == mvolRootDeviceObject) {
-		printk(KERN_DEBUG "Root device request.\n");
+		dbg(KERN_DEBUG "Root device request.\n");
 
 		irp->IoStatus.Status = STATUS_SUCCESS;
 	        IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -1089,7 +1089,7 @@ static NTSTATUS windrbd_pnp(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 	NTSTATUS status;
 
 	if (device == mvolRootDeviceObject) {
-		printk(KERN_DEBUG "Root device request.\n");
+		dbg(KERN_DEBUG "Root device request.\n");
 
 		irp->IoStatus.Status = STATUS_SUCCESS;
 	        IoCompleteRequest(irp, IO_NO_INCREMENT);
