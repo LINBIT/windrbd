440d26e054f2017dbe1d668684256c1c2336c491
Johannes Thoma <johannes@johannesthoma.com>
2018-11-21 14:54:54 +0100

    Fixed pre-include for util.c
    It will go away soon, however includes should be fixed now.

--- win4lin/src/util.c
+++ win4lin/src/util.c
@@ -20,11 +20,11 @@
 */
 
 // #include <Ntifs.h>
-#include <Ntstrsafe.h>
 #include <ntddk.h>
 #include <stdlib.h>
 #include <Mountmgr.h>
 #include <ntddvol.h>
+#include <Ntstrsafe.h>
 
 #include "drbd_windows.h"
 #include "drbd_wingenl.h"
