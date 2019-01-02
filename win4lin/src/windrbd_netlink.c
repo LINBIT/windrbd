88a937600c2dc8454523efde293b89c295a50c58
Johannes Thoma <johannes@johannesthoma.com>
2019-01-02 18:36:09 +0100

    Can create new minor on boot.
    Need to have before / after generic func to save LOCs.

--- win4lin/src/windrbd_netlink.c
+++ win4lin/src/windrbd_netlink.c
@@ -579,12 +579,8 @@ static void fill_in_header(struct sk_buff *skb)
 
 extern struct genl_family drbd_genl_family;
 
-int windrbd_create_boot_device(void)
+static int reply_code(int cmd)
 {
-	struct sk_buff *skb;
-	int ret;
-	struct nlattr *nla;
-	struct drbd_genlmsghdr *dhdr;
 	char reply[NLMSG_GOODSIZE];
 	size_t reply_size;
 	int i;
@@ -592,10 +588,51 @@ int windrbd_create_boot_device(void)
 	struct genlmsghdr *genlmsg_header;
 	struct drbd_genlmsghdr *drbd_header;
 
-	const char *resource_name = "w0";
-	int my_node_id = 2;
+#define MIN_REPLY_SIZE (NLMSG_HDRLEN+GENL_HDRLEN+NLMSG_ALIGN(sizeof(struct drbd_genlmsghdr)))
 
-        drbd_genl_family.id = WINDRBD_NETLINK_FAMILY_ID;
+	for (i=0;i<100;i++) {
+		reply_size = windrbd_receive_netlink_packets(reply, sizeof(reply), KERNEL_PORT_ID);
+		if (reply_size > 0)
+			break;
+
+		msleep(100);
+	}
+	if (reply_size == 0) {
+		printk("Timeout waiting for netlink reply packet.\n");
+		return -1;
+	}
+	if (reply_size < MIN_REPLY_SIZE) {
+		printk("Reply too small.\n");
+		return -1;
+	}
+
+	header = (struct nlmsghdr*) reply;
+	genlmsg_header = nlmsg_data(header);
+	drbd_header = genlmsg_data(genlmsg_header);
+
+	if (header->nlmsg_seq != expected_seq) {
+		printk("Warning: header->nlmsg_seq(%d) != expected_seq(%d)\n", header->nlmsg_seq, expected_seq);
+		return -1;
+	}
+	expected_seq++;
+	if (header->nlmsg_pid != KERNEL_PORT_ID) {
+		printk("Warning: header->nlmsg_pid(%d) != KERNEL_PORT_ID\n", header->nlmsg_pid, KERNEL_PORT_ID);
+		return -1;
+	}
+	if (genlmsg_header->cmd != cmd) {
+		printk("Warning: genlmsg_header->cmd(%d) != DRBD_ADM_NEW_RESOURCE (%d)\n", genlmsg_header->cmd, cmd);
+		return -1;
+	}
+
+	return drbd_header->ret_code;
+}
+
+static int new_resource(const char *resource_name, int my_node_id)
+{
+	struct sk_buff *skb;
+	struct nlattr *nla;
+	struct drbd_genlmsghdr *dhdr;
+	int ret, rc;
 
 	skb = genlmsg_new(NLMSG_GOODSIZE, GFP_KERNEL);
 	if (skb == NULL)
@@ -614,39 +651,69 @@ int windrbd_create_boot_device(void)
 	nla_nest_end(skb, nla);
 
 	fill_in_header(skb);
-
 	ret = windrbd_process_netlink_packet(&skb->data, skb->tail);
 
 	nlmsg_free(skb);
 
-	for (i=0;i<100;i++) {
-		reply_size = windrbd_receive_netlink_packets(reply, sizeof(reply), KERNEL_PORT_ID);
-		if (reply_size > 0)
-			break;
+	if (ret != 0)
+		return ret;
 
-		msleep(100);
+	rc = reply_code(DRBD_ADM_NEW_RESOURCE);
+	if (rc != NO_ERROR) {
+		printk("status is %d\n", rc);
+		return rc;
 	}
-	if (reply_size == 0) {
-		printk("Timeout waiting for netlink packet.\n");
-	} else {
-printk("i: %d\n", i);
-		header = (struct nlmsghdr*) reply;
-		genlmsg_header = nlmsg_data(header);
-		drbd_header = genlmsg_data(genlmsg_header);
+	return 0;
+}
 
-		if (header->nlmsg_seq != expected_seq)
-			printk("Warning: header->nlmsg_seq(%d) != expected_seq(%d)\n", header->nlmsg_seq, expected_seq);
-		expected_seq++;
-		if (header->nlmsg_pid != KERNEL_PORT_ID)
-			printk("Warning: header->nlmsg_pid(%d) != KERNEL_PORT_ID\n", header->nlmsg_pid, KERNEL_PORT_ID);
-		if (genlmsg_header->cmd != DRBD_ADM_NEW_RESOURCE)
-			printk("Warning: genlmsg_header->cmd(%d) != DRBD_ADM_NEW_RESOURCE (%d)\n", genlmsg_header->cmd, DRBD_ADM_NEW_RESOURCE);
+static int new_minor(const char *resource_name, int minor, int volume)
+{
+	struct sk_buff *skb;
+	struct nlattr *nla;
+	struct drbd_genlmsghdr *dhdr;
+	int ret, rc;
 
-		printk("minor: %d ret_code: %d\n", drbd_header->minor, drbd_header->ret_code);
-/*		for (i=0;i<reply_size;i++) {
-			printk("reply[%d]: %x(%c)\n", i, (unsigned char) reply[i], (unsigned char) reply[i]);
-		} */
+	skb = genlmsg_new(NLMSG_GOODSIZE, GFP_KERNEL);
+	if (skb == NULL)
+		return -ENOMEM;
+
+        dhdr = genlmsg_put(skb, 0, seq, &drbd_genl_family, 0, DRBD_ADM_NEW_MINOR
+);
+	dhdr->minor = minor;
+	dhdr->flags = 0;
+
+	nla = nla_nest_start(skb, DRBD_NLA_CFG_CONTEXT);
+	nla_put_string(skb, T_ctx_resource_name, resource_name);
+	nla_put_u32(skb, T_ctx_volume, volume);
+	nla_nest_end(skb, nla);
+
+	fill_in_header(skb);
+	ret = windrbd_process_netlink_packet(&skb->data, skb->tail);
+
+	nlmsg_free(skb);
+
+	if (ret != 0)
+		return ret;
+
+	rc = reply_code(DRBD_ADM_NEW_MINOR);
+	if (rc != NO_ERROR) {
+		printk("status is %d\n", rc);
+		return rc;
 	}
+	return 0;
+}
+
+int windrbd_create_boot_device(void)
+{
+	int ret;
+
+        drbd_genl_family.id = WINDRBD_NETLINK_FAMILY_ID;
+
+	if ((ret = new_resource("w0", 2)) != 0)
+		return ret;
+
+	if ((ret = new_minor("w0", 5, 17)) != 0)
+		return ret;
 
 	return ret;
 }
