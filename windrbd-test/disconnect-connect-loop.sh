62311f58a6006c81cb9ffd5457f635ed47347f1f
Johannes Thoma <johannes@johannesthoma.com>
2019-02-01 16:38:56 +0100

    Test update

--- windrbd-test/disconnect-connect-loop.sh
+++ windrbd-test/disconnect-connect-loop.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
 i=0
 while true
 do
 	i=$[ $i+1 ]
-	echo Disconnect/Connect $i
+	echo Disconnect $i
 	drbdadm disconnect w0
 	sleep 5
+	echo Connect $i
 	drbdadm connect w0
 	sleep 5 
 done
