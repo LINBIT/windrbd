5a4984979a23ba8de67e5efec7f28e2a5d9bcf54
Johannes Thoma <johannes@johannesthoma.com>
2019-02-21 21:07:37 +0100

    Unload test refined.
    No BSOD when just doing attach or status. There is some resource
    leak in networking code (even when we don't connect and bring
    everything down).

--- windrbd-test/load-unload-test.sh
+++ windrbd-test/load-unload-test.sh
@@ -1,8 +1,22 @@
+#!/bin/bash
+
 RES=${RES:-w0}
 i=0
 while true
 do
-	drbdadm up $RES
+	i=$[ $i+1 ]
+	echo "Load/Unload $i"
+# Currently BSODs with this:
+#	drbdadm up $RES
+# No BSOD with this (10 minutes test)
+#	drbdadm status
+# Now trying with disk only (no connect)
+	drbdsetup new-resource w0 2
+	drbdsetup new-minor w0 5 17
+	drbdmeta 5 v09 G: flex-external apply-al
+	drbdsetup attach 5 F: G: flexible
+	drbdadm status
+
 	sleep 2
 	drbdadm down all
 	sc stop windrbdumhelper
