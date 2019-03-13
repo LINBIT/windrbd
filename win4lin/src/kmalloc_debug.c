dca6d6254aaee5df1718b5c1218b57308dbccc70
Johannes Thoma <johannes@johannesthoma.com>
2019-03-13 18:54:40 +0100

    Reverted kmalloc_debug again.
    It works now (loading the driver), carefully picking the order
    of initialization solved the BSOD on driver startup. Spinlock
    debugging now can be used in conjunction with kmalloc debugging.

--- win4lin/src/kmalloc_debug.c
+++ win4lin/src/kmalloc_debug.c
@@ -35,7 +35,6 @@ void *kmalloc_debug(size_t size, int flag, const char *file, int line, const cha
 	size_t full_size;
 	ULONG_PTR flags;
 
-#if 0
 	if (kmalloc_errors > 0) {
 
 			/* Don't print all the time, since printk itself
@@ -46,13 +45,10 @@ void *kmalloc_debug(size_t size, int flag, const char *file, int line, const cha
 			printk("%d kmalloc errors so far (%d)\n", kmalloc_errors, print_kmalloc_error);
 		print_kmalloc_error++;
 	}
-#endif
 
 	full_size = sizeof(struct memory) + size + sizeof(struct poison_after);
 	mem = ExAllocatePoolWithTag(NonPagedPool, full_size, 'DRBD');
 
-if (mem == NULL) return NULL;
-#if 0
 	if (mem == NULL) {
 		if (strcmp(func, "SendTo") != 0)
 			printk("kmalloc_debug: Warning: cannot allocate memory of size %d, %d bytes requested by function %s at %s:%d.\n", full_size, size, func, file, line);
@@ -69,13 +65,9 @@ if (mem == NULL) return NULL;
 	poison_after = (struct poison_after*) (&mem->data[size]);
 	poison_after->poison2 = POISON_AFTER;
 
-// printk("memory_lock.printk_lock is %d\n", memory_lock.printk_lock);
-/*
 	spin_lock_irqsave(&memory_lock, flags);
 	list_add(&mem->list, &memory_allocations);
 	spin_unlock_irqrestore(&memory_lock, flags);
-*/
-#endif
 
 	return &mem->data[0];
 }
@@ -97,14 +89,11 @@ void kfree_debug(const void *data, const char *file, int line, const char *func)
 	struct poison_after *poison_after;
 	ULONG_PTR flags;
 
-#if 0
 	if (data == NULL) {
 		printk("kmalloc_debug: Warning: attempt to free the NULL pointer in function %s at %s:%d\n", func, file, line);
 		return;
 	}
-#endif
 	mem = container_of((void*) data, struct memory, data);
-#if 0
 	poison_after = (struct poison_after*) (&mem->data[mem->size]);
 
 	if (mem->poison != POISON_BEFORE)
@@ -112,13 +101,10 @@ void kfree_debug(const void *data, const char *file, int line, const char *func)
 	if (poison_after->poison2 != POISON_AFTER)
 		printk("kmalloc_debug: Warning: Poison before overwritten (is %x should be %x)\n", poison_after->poison2, POISON_AFTER);
 
-/*
 	spin_lock_irqsave(&memory_lock, flags);
 	list_del(&mem->list);
 	spin_unlock_irqrestore(&memory_lock, flags);
-*/
 
-#endif
 	ExFreePool((void*)mem);
 }
 
