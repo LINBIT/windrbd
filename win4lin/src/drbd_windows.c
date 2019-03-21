5ccf00ada0f838dac3420013510587a93b4ee486
Johannes Thoma <johannes@johannesthoma.com>
2019-03-21 18:22:57 +0100

    Removed add/remove spinlock again.
    We had a BSOD but not sure if it was the compile process.

--- win4lin/src/drbd_windows.c
+++ win4lin/src/drbd_windows.c
@@ -1420,12 +1420,12 @@ static int bad_spinlock_test_thread(void *unused)
 
 int spinlock_debug_init(void)
 {
+/*
 	run_spinlock_monitor = 1;
 	if (kthread_run(see_all_spinlocks_thread, NULL, "spinlock_debug") == NULL) {
 		printk("Warning: could not start spinlock monitor\n");
 		return -1;
 	}
-/*
 	if (kthread_run(bad_spinlock_test_thread, NULL, "spinlock_test") == NULL) {
 		printk("Warning: could not start spinlock test\n");
 		return -1;
@@ -1449,8 +1449,10 @@ long _spin_lock_irqsave_debug(spinlock_t *lock, const char *file, int line, cons
 {
 	KIRQL oldIrql;
 
+/*
 	if (!lock->printk_lock)
 		add_spinlock(lock, file, line, func);
+*/
 	KeAcquireSpinLock(&lock->spinLock, &oldIrql);
 
 	return (long)oldIrql;
@@ -1459,8 +1461,10 @@ long _spin_lock_irqsave_debug(spinlock_t *lock, const char *file, int line, cons
 void spin_unlock_irqrestore_debug(spinlock_t *lock, long flags, const char *file, int line, const char *func)
 {
 	KeReleaseSpinLock(&lock->spinLock, (KIRQL) flags);
+/*
 	if (!lock->printk_lock)
 		remove_spinlock(lock);
+*/
 }
 
 void spin_lock_irq_debug(spinlock_t *lock, const char *file, int line, const char *func)
@@ -1470,14 +1474,18 @@ void spin_lock_irq_debug(spinlock_t *lock, const char *file, int line, const cha
 	if (KeGetCurrentIrql() != PASSIVE_LEVEL)
 		printk("spin lock bug: KeGetCurrentIrql() is %d (called from %s:%d in %s()\n", KeGetCurrentIrql(), file, line, func);
 
+/*
 	add_spinlock(lock, file, line, func);
+*/
 	KeAcquireSpinLock(&lock->spinLock, &unused);
 }
 
 void spin_unlock_irq_debug(spinlock_t *lock, const char *file, int line, const char *func)
 {
 	KeReleaseSpinLock(&lock->spinLock, PASSIVE_LEVEL);
+/*
 	remove_spinlock(lock);
+*/
 }
 
 void spin_lock_debug(spinlock_t *lock, const char *file, int line, const char *func)
