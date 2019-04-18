30053fd18bf62253aacb5af5d34b547e1e3afe0e
Johannes Thoma <johannes@johannesthoma.com>
2019-04-18 15:41:58 +0200

    tmp patch: Name Windows (upper) device HarddiskVolume<minor>
    This device name is coded into the boot config, it seems
    that Windows is looking first for \\Device\\HarddiskVolume1
    before looking for C:. This patch makes WinDRBD (temporary)
    unusable for VMs that have a disk (behaviour is undefined).

--- win4lin/src/drbd_windows.c
+++ win4lin/src/drbd_windows.c
@@ -3245,7 +3245,9 @@ static int minor_to_windows_device_name(UNICODE_STRING *name, int minor)
 	name->Length = 0;
 	name->MaximumLength = (len - 1) * sizeof(name->Buffer[0]);
 
-	status = RtlUnicodeStringPrintf(name, L"\\Device\\Drbd%d", minor);
+	// status = RtlUnicodeStringPrintf(name, L"\\Device\\Drbd%d", minor);
+		/* TODO: tmp patch for diskless boot */
+	status = RtlUnicodeStringPrintf(name, L"\\Device\\HarddiskVolume%d", minor);
 
 	if (status != STATUS_SUCCESS) {
 		WDRBD_WARN("minor_to_dos_name: couldn't printf device name for minor %d status: %x\n", minor, status);
