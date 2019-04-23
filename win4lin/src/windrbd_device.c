127985cd28174716dd38640a9cc630ad3821e7d8
Johannes Thoma <johannes@johannesthoma.com>
2019-04-23 20:10:16 +0200

    Make lower device handle device relations != BusRelations.
    The problem currently is that we never get BusRelations
    query. Probably we really need to call IoInvalidateDeviceRelations()
    in order to get those sent.

--- win4lin/src/windrbd_device.c
+++ win4lin/src/windrbd_device.c
@@ -1253,7 +1253,7 @@ printk("NOT completing IRP\n");
 		dbg("got IRP_MN_QUERY_DEVICE_RELATIONS\n");
 
 		int type = s->Parameters.QueryDeviceRelations.Type;
-dbg("Pnp: Is a IRP_MN_QUERY_DEVICE_RELATIONS: s->Parameters.QueryDeviceRelations.Type is %x\n", s->Parameters.QueryDeviceRelations.Type);
+dbg("Pnp: Is a IRP_MN_QUERY_DEVICE_RELATIONS: s->Parameters.QueryDeviceRelations.Type is %x (bus relations is %x)\n", s->Parameters.QueryDeviceRelations.Type, BusRelations);
 		if (s->Parameters.QueryDeviceRelations.Type == BusRelations) {
 printk("about to report DRBD devices ...\n");
 			int num_devices = get_all_drbd_device_objects(NULL, 0);
@@ -1273,7 +1273,7 @@ printk("about to report DRBD devices ...\n");
 			irp->IoStatus.Status = STATUS_SUCCESS;
 			return STATUS_SUCCESS;
 		} else {
-			return STATUS_NOT_IMPLEMENTED;
+			status = STATUS_SUCCESS;
 		}
 		break;
 
