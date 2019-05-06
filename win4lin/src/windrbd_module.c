3d059de0bf74c0964eecda76422d605ed2f67865
Johannes Thoma <johannes@johannesthoma.com>
2019-05-06 16:37:37 +0200

    module: do not touch AddDevice.
    We need it now for implementing the PnP mechanism. Maybe later
    revert this patch to allow for driver unloading again.

--- win4lin/src/windrbd_module.c
+++ win4lin/src/windrbd_module.c
@@ -18,8 +18,11 @@ bool try_module_get(struct module *module)
 	}
 
 	if (atomic_inc_return(&module->refcnt) == 1) {
+/*
 		printk("Locking module by setting AddDevice to %p, sc stop windrbd should not work (do a drbdadm down all first)\n", mvolAddDevice);
 		mvolDriverObject->DriverExtension->AddDevice = mvolAddDevice;
+*/
+		printk("not touching AddDevice\n");
 	}
 	return true;
 }
@@ -32,8 +35,11 @@ void module_put(struct module *module)
 	}
 
 	if (atomic_dec_return(&module->refcnt) == 0) {
+/*
 		printk("Unlocking module by setting AddDevice to NULL, sc stop windrbd should work now.\n");
 		mvolDriverObject->DriverExtension->AddDevice = NULL;
+*/
+		printk("not touching AddDevice\n");
 	}
 }
 
