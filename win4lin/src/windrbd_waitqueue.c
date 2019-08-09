343a29e9e7e536af8fb6174e99aff9284a41403c
Johannes Thoma <johannes@johannesthoma.com>
2019-08-09 16:15:43 +0200

    Tmp commit: try with maximum timeout 100 ms.
    This should be reverted as soon as possible.

--- win4lin/src/windrbd_waitqueue.c
+++ win4lin/src/windrbd_waitqueue.c
@@ -20,6 +20,9 @@ static int ll_wait(wait_queue_head_t *q, ULONG_PTR timeout, int interruptible, c
 	PVOID wait_objects[2] = {0};
 	struct task_struct *thread = current;
 
+	/* Busy looping .. to see where it hangs */
+if (timeout > 100) timeout = 100;
+
 	if(timeout != MAX_SCHEDULE_TIMEOUT) {
 		wait_time.QuadPart = timeout * (-1 * 1000 * 1000 * 10 / HZ);
 		wait_time_p = &wait_time;
