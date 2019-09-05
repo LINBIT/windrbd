ea51e91243791aa2a1e87ba578c41b882d9b5187
Johannes Thoma <johannes@johannesthoma.com>
2019-09-05 16:38:35 +0200

    Sane defaults for network timeouts.
    Before that patch they were undefined causing WinDRBD not
    being able to connect at boot time (it hung after loading
    drivers). We need to check if that patch fixes our
    boot problems but I assume yes.

--- win4lin/src/windrbd_bootdevice.c
+++ win4lin/src/windrbd_bootdevice.c
@@ -179,6 +179,10 @@ static int new_peer(const char *resource_name, const char *peer_name, int peer_n
 	nla_put_string(skb, T_verify_alg, "crc32c");
 	nla_put_u32(skb, T_rcvbuf_size, 0xa00000);
 	nla_put_u32(skb, T_sndbuf_size, 0xa00000);
+	nla_put_u32(skb, T_timeout, 6*10);
+	nla_put_u32(skb, T_ping_timeo, 10*10);
+	nla_put_u32(skb, T_ping_int, 10);
+	nla_put_u32(skb, T_connect_int, 10);
 	nla_put_u8(skb, T_use_rle, 0);
 	nla_put_u32(skb, T_wire_protocol, protocol);
 	nla_nest_end(skb, nla);
