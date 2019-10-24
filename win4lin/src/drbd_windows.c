f084d264ba5a719449aab3315bf8ec11c1e3c249
Johannes Thoma <johannes@johannesthoma.com>
2019-10-24 13:46:28 +0200

    Clear the device_removed event when creating windows device.
    On drbdadm primary / secondary / primary / secondary, the last
    secondary does not wait for the device being removed by the
    PnP engine. This may be the cause for some odd behaviour we
    observed (BSODs, ...).

--- win4lin/src/drbd_windows.c
+++ win4lin/src/drbd_windows.c
@@ -3427,6 +3427,7 @@ int windrbd_create_windows_device(struct block_device *bdev)
 		 */
 	bdev->delete_pending = false;
 	bdev->about_to_delete = false;
+	KeClearEvent(&bdev->device_removed_event);
 
 	bdev_ref = new_device->DeviceExtension;
 	bdev_ref->bdev = bdev;
