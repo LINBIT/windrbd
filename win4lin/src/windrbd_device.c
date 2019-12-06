56d6c729c9e021b70d405710a86c214a885b3551
Johannes Thoma <johannes@johannesthoma.com>
2019-12-06 19:48:35 +0100

    Not waiting for primary on QUERY_PROPERTY.
    This should fix the PNP_WATCHDOG BSOD but it doesn't. Now,
    the ioctl never gets called.

--- win4lin/src/windrbd_device.c
+++ win4lin/src/windrbd_device.c
@@ -399,8 +399,9 @@ static NTSTATUS windrbd_device_control(struct _DEVICE_OBJECT *device, struct _IR
 	struct _IO_STACK_LOCATION *s = IoGetCurrentIrpStackLocation(irp);
 	NTSTATUS status = STATUS_SUCCESS;
 
-// printk("ioctl is %x\n", s->Parameters.DeviceIoControl.IoControlCode);
-	if (dev->is_bootdevice) {
+dbg("ioctl is %x\n", s->Parameters.DeviceIoControl.IoControlCode);
+	if (dev->is_bootdevice && 
+	    s->Parameters.DeviceIoControl.IoControlCode != IOCTL_STORAGE_QUERY_PROPERTY) {
 		status = wait_for_becoming_primary(dev);
 		if (status != STATUS_SUCCESS)
 			goto out;
