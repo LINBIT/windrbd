a78f623b2e50b10caa5ee6290e5231f7e362b98f
Johannes Thoma <johannes@johannesthoma.com>
2020-01-20 20:06:00 +0100

    Test: iptables drop / dont drop loop for WinDRBD booted VM.
    Test shows that connection is re-established, however machine
    is unresponsive (something loops)

--- windrbd-test/iptables-drop-dont-drop-loop.sh
+++ windrbd-test/iptables-drop-dont-drop-loop.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # run as root on a Linux host port is hardcoded to 7600
 i=0
+PORT=7695
+RES=windows10-boot
+
 while true
 do 
 	i=$[ $i+1 ]
 	echo "network failure $i"
 	echo "about to drop packets"
-	./iptables-drop.sh
-	sleep 10
+	./iptables-drop-port.sh $PORT
+	sleep 15
 	drbdadm status
 	echo "about to not drop packets"
-	./iptables-dont-drop.sh
-	sudo drbdadm wait-sync w0
+	./iptables-dont-drop-port.sh $PORT
+	sudo drbdadm wait-sync $RES
 	sleep 2
 	drbdadm status
 done
