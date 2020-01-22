fdead81431d2bfda62b8be8e8c99a4141ba36db1
Johannes Thoma <johannes@johannesthoma.com>
2020-01-22 13:13:02 +0100

    Test for my virtualbox VMs (iptables)

--- windrbd-test/iptables-drop-dont-drop-loop.sh
+++ windrbd-test/iptables-drop-dont-drop-loop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # run as root on a Linux host port is hardcoded to 7600
 i=0
-PORT=7695
-RES=windows10-boot
+PORT=7600
+RES=w0
+# PORT=7695
+# RES=windows10-boot
 
 while true
 do 
@@ -10,7 +12,7 @@ do
 	echo "network failure $i"
 	echo "about to drop packets"
 	./iptables-drop-port.sh $PORT
-	sleep 15
+	sleep 20	# ping timeout
 	drbdadm status
 	echo "about to not drop packets"
 	./iptables-dont-drop-port.sh $PORT
