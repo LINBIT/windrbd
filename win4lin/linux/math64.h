f6d55d06254b1102a8242e9bab59ad5dac9615ed
Johannes Thoma <johannes@johannesthoma.com>
2020-03-18 21:10:43 +0100

    Added math64 header.

---
+++ win4lin/linux/math64.h
@@ -0,0 +1,18 @@
+#ifndef _MATH64_H
+#define _MATH64_H
+
+#include <linux/types.h>
+
+static inline u64 div_u64_rem(u64 dividend, u32 divisor, u32 *remainder)
+{
+	*remainder = dividend % divisor;
+	return dividend / divisor;
+}
+
+static inline u64 div_u64(u64 dividend, u32 divisor)
+{
+	u32 remainder;
+	return div_u64_rem(dividend, divisor, &remainder);
+}
+
+#endif
