4b52059340a4bf688ebd20ce0cb85ed24b178cc3
Johannes Thoma <johannes@johannesthoma.com>
2020-04-09 22:27:26 +0200

    hash: Only support crc32c, cleaned up.

--- win4lin/crypto/hash.h
+++ win4lin/crypto/hash.h
@@ -1,108 +1,80 @@
-#ifndef _WDRBD_CRYPTO_HASH_H
-#define _WDRBD_CRYPTO_HASH_H
+#ifndef _CRYPTO_HASH_H
+#define _CRYPTO_HASH_H
 
 #include <wdm.h>
 #include <drbd_windows.h>
 
+/* We only support crc32c */
 
 #define CRYPTO_ALG_ASYNC 4711
 #define CRYPTO_ALG_TYPE_HASH CRYPTO_ALG_TYPE_DIGEST
 
 struct crypto_shash {
-	struct crypto_tfm *base;
-	const u8 *key;
+	const uint8_t *key;
 	int keylen;
 };
 
-struct hash_desc {
+struct shash_desc {
+	uint32_t crc;
 	struct crypto_shash *tfm;
-	u32 flags;
 };
+
+#define SHASH_DESC_ON_STACK(shash, ctx)				  \
+	char __##shash##_desc[sizeof(struct shash_desc)];	  \
+	struct shash_desc *shash = (struct shash_desc *)__##shash##_desc
 
 static inline struct crypto_shash *
 crypto_alloc_shash(const char *alg_name, u32 type, u32 mask)
 {
 	struct crypto_shash *ch;
-	static const char supported[] = "crc32c";
-	const int supported_len = sizeof(supported);
 
-	if (RtlCompareMemory(alg_name, supported, supported_len) != supported_len)
+	if (strcmp(alg_name, "crc32c") != 0)
 		return ERR_PTR(-EOPNOTSUPP);
 
-	ch = kmalloc(sizeof(struct crypto_shash), GFP_KERNEL, 'HSWD');
+	ch = kmalloc(sizeof(*ch), GFP_KERNEL, 'HSWD');
 	if (!ch)
 		return ERR_PTR(-ENOMEM);
 
 	return ch;
 }
 
-static inline int
-crypto_hash_setkey(struct crypto_shash *hash, const u8 *key, unsigned int keylen)
-{
-	hash->key = key;
-	hash->keylen = keylen;
-
-	return 0;
-}
-
-static inline int
-crypto_hash_digest(struct hash_desc *desc, struct scatterlist *sg,
-		   unsigned int nbytes, u8 *out)
-{
-#if 0
-	// TODO reimplement!
-	crypto_hmac(desc->tfm->base, (u8 *)desc->tfm->key,
-		    &desc->tfm->keylen, sg, 1 /* ! */ , out);
-	/* ! this is not generic. Would need to convert nbytes -> nsg */
-#endif
-	return 0;
-}
-
 static inline void crypto_free_shash(struct crypto_shash *tfm)
 {
-	if (!tfm)
-		return;
-
 	kfree(tfm);
 }
 
 static inline unsigned int crypto_shash_digestsize(struct crypto_shash *tfm)
 {
-	return crypto_tfm_alg_digestsize(tfm->base);
+	return 4;
 }
 
-static inline struct crypto_tfm *crypto_hash_tfm(struct crypto_shash *tfm)
+static inline int crypto_shash_init(struct shash_desc *desc)
 {
-	return tfm->base;
-}
+	desc->crc = 0;
 
-static inline int crypto_hash_init(struct hash_desc *desc)
-{
 	return 0;
 }
 
-static inline int crypto_hash_update(struct hash_desc *desc,
-				     struct scatterlist *sg,
-				     unsigned int nbytes)
+static inline int crypto_shash_update(struct shash_desc *desc,
+				      uint8_t *data,
+				      unsigned int nbytes)
 {
-	*(int*)desc = crc32c(0, (uint8_t *)sg, nbytes);
+	desc->crc = crc32c(desc->crc, data, nbytes);
+
 	return 0;
 }
 
-static inline int crypto_hash_final(struct hash_desc *desc, u8 *out)
+static inline int crypto_shash_final(struct shash_desc *desc, uint8_t *out)
 {
-	int i;
-	u8 *p = (u8*)desc;
-	for(i = 0; i < 4; i++)
-	{
-		*out++ = *p++; // long
-	}
+	*((uint32_t*)out) = desc->crc;
+
 	return 0;
 }
 
+static inline void shash_desc_zero(struct shash_desc *desc)
+{
+	desc->crc = 0;
+}
 
-#define crypto_ahash			crypto_shash
-#define crypto_ahash_digestsize		crypto_shash_digestsize
-#define crypto_alloc_ahash		crypto_alloc_shash
-#define crypto_free_ahash               crypto_free_shash
+
 #endif
