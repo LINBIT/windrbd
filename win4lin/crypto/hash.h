c439626b82cdba5472a04d1a373342334969f177
Johannes Thoma <johannes@johannesthoma.com>
2020-04-11 22:23:12 +0200

    Attempt to fix it with different init value.
    The Linux kernel crc32c computes 0x98f94189 as the crc32c
    for 4096 0's. This patch does not fix online verify.

--- win4lin/crypto/hash.h
+++ win4lin/crypto/hash.h
@@ -50,7 +50,8 @@ static inline unsigned int crypto_shash_digestsize(struct crypto_shash *tfm)
 
 static inline int crypto_shash_init(struct shash_desc *desc)
 {
-	desc->crc = 0;
+	// desc->crc = 0;
+	desc->crc = 0x98f94189;
 
 	return 0;
 }
