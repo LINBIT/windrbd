24cbdec2c192266ddfd9ac3c18e9d14d8b0920b9
Johannes Thoma <johannes@johannesthoma.com>
2020-04-15 15:24:21 +0200

    Removed printk from hash.h header.

--- win4lin/crypto/hash.h
+++ win4lin/crypto/hash.h
@@ -50,10 +50,7 @@ static inline unsigned int crypto_shash_digestsize(struct crypto_shash *tfm)
 
 static inline int crypto_shash_init(struct shash_desc *desc)
 {
-	// desc->crc = 0;
-	// desc->crc = 0x98f94189;
 	desc->crc = 0xffffffff;
-printk("initializing hash to %x\n", desc->crc);
 
 	return 0;
 }
