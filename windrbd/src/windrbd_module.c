f614f0630a58ce1a9d9fee7868c1173aac7f224a
Johannes Thoma <johannes@johannesthoma.com>
2020-08-04 17:51:40 +0200

    Do not touch AddDevice.
    Might be the reason for upgrade BSOD.

--- windrbd/src/windrbd_module.c
+++ windrbd/src/windrbd_module.c
@@ -18,9 +18,14 @@ bool try_module_get(struct module *module)
 	}
 
 	if (atomic_inc_return(&module->refcnt) == 1) {
+#if 0
 		printk("Locking module by setting AddDevice to %p, sc stop windrbd should not work (do a drbdadm down all first)\n", mvolAddDevice);
 		mvolDriverObject->DriverExtension->AddDevice = mvolAddDevice;
+#else
+		printk("Would lock driver now.\n");
+#endif
 	}
+printk("module->refcnt is %d\n", atomic_read(&module->refcnt));
 	return true;
 }
 
@@ -32,9 +37,14 @@ void module_put(struct module *module)
 	}
 
 	if (atomic_dec_return(&module->refcnt) == 0) {
+#if 0
 		printk("Unlocking module by setting AddDevice to NULL, sc stop windrbd should work now.\n");
 		mvolDriverObject->DriverExtension->AddDevice = NULL;
+#else
+		printk("Would unlock driver now.\n");
+#endif
 	}
+printk("module->refcnt is %d\n", atomic_read(&module->refcnt));
 }
 
 
