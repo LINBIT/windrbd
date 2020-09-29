e91656a4d92de7c70f7f2d482227e87bc5ee6b91
Johannes Thoma <johannes@johannesthoma.com>
2020-09-29 13:25:07 +0200

    Workqueue thread has workqueue name now.
    One day we implement varargs for kthread_run() ...

--- windrbd/src/drbd_windows.c
+++ windrbd/src/drbd_windows.c
@@ -1104,7 +1104,7 @@ struct workqueue_struct *alloc_ordered_workqueue(const char * fmt, int flags, ..
 
 // printk("starting a workqueue thread\n");
 
-	wq->thread = kthread_run(run_singlethread_workqueue, wq, "workqueue");
+	wq->thread = kthread_run(run_singlethread_workqueue, wq, wq->name);
 	if (IS_ERR(wq->thread)) {
 		printk("kthread_run failed on creating workqueue thread, err is %d\n", PTR_ERR(wq->thread));
 		kfree(wq);
