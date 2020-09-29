b06d1ec81fb3703c717ac9f1018f3179d287aa75
Johannes Thoma <johannes@johannesthoma.com>
2020-09-29 15:49:40 +0200

    Reverted workqueue thread creation.
    We had strange BSODs (in w->cb of process_sender_todo()) but
    maybe these come from somewhere else (we had them before also).

--- windrbd/src/drbd_windows.c
+++ windrbd/src/drbd_windows.c
@@ -1104,13 +1104,14 @@ struct workqueue_struct *alloc_ordered_workqueue(const char * fmt, int flags, ..
 
 // printk("starting a workqueue thread\n");
 
-	wq->thread = kthread_create(run_singlethread_workqueue, wq, "wq_%s", wq->name);
+	// wq->thread = kthread_create(run_singlethread_workqueue, wq, "wq_%s", wq->name);
+	wq->thread = kthread_run(run_singlethread_workqueue, wq, "workqueue");
 	if (IS_ERR(wq->thread)) {
 		printk("kthread_run failed on creating workqueue thread, err is %d\n", PTR_ERR(wq->thread));
 		kfree(wq);
 		return NULL;
 	}
-	wake_up_process(wq->thread);
+//	wake_up_process(wq->thread);
 
 	return wq;
 }
