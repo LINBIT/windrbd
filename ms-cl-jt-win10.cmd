f0d2108ed0e866abcbfbe44f5af2f7f472aa9ad2
Johannes Thoma <johannes@johannesthoma.com>
2020-11-20 14:05:03 +0100

    Changed location of EWDK on windows 10 box.

--- ms-cl-jt-win10.cmd
+++ ms-cl-jt-win10.cmd
@@ -1,4 +1,4 @@
-set EWDK_BASE=d:
+set EWDK_BASE=e:
 call %EWDK_BASE%\BuildEnv\SetupBuildEnv.cmd amd64
 set PATH=%PATH%;"D:\Program Files\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.14.26428\bin\Hostx64\x64"
 cl.exe %*
