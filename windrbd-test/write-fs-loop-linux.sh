66168506627c8b0a2d611a0b7b01aa37112302e0
Johannes Thoma <johannes@johannesthoma.com>
2020-12-01 18:09:47 +0100

    Time iterations on writing.
    Without peer: 1 sec with WinDRBD peer 10-15 secs .. performance leak
    (CPU at 100%)

--- windrbd-test/write-fs-loop-linux.sh
+++ windrbd-test/write-fs-loop-linux.sh
@@ -6,7 +6,9 @@ do
 	i=$[ $i+1 ]
 	echo Write File $i
 #	cp /dev/zero /home/johannes/Linbit/tmp/mnt/data
-	dd if=/dev/zero of=/home/johannes/Linbit/tmp/mnt/data bs=$[ 1024*1024 ] count=30
-	sync
+	time (
+		dd if=/dev/zero of=/home/johannes/Linbit/tmp/mnt/data bs=$[ 1024*1024 ] count=30
+		sync
+	)
 	date
 done
