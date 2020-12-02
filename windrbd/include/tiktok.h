d1bc188b390b6c095917b69b577b13bc62b5270e
Johannes Thoma <johannes@johannesthoma.com>
2020-12-02 16:12:56 +0100

    Tiktok channels.
    So we can measure more code ranges at once.

--- windrbd/include/tiktok.h
+++ windrbd/include/tiktok.h
@@ -1,10 +1,10 @@
 #ifndef _TIKTOK_H
 #define _TIKTOK_H
 
-void tik_debug(const char *file, int line, const char *func);
-void tok_debug(const char *file, int line, const char *func);
+void tik_debug(int n, const char *file, int line, const char *func);
+void tok_debug(int n, const char *file, int line, const char *func);
 
-#define tik() do { tik_debug(__FILE__, __LINE__, __func__); } while (0);
-#define tok() do { tok_debug(__FILE__, __LINE__, __func__); } while (0);
+#define tik(n) do { tik_debug((n), __FILE__, __LINE__, __func__); } while (0);
+#define tok(n) do { tok_debug((n), __FILE__, __LINE__, __func__); } while (0);
 
 #endif
--- windrbd/src/tiktok.c
+++ windrbd/src/tiktok.c
@@ -1,25 +1,40 @@
 #include <wdm.h>
 #include "drbd_windows.h"
 
-static LARGE_INTEGER hr_timer;
+#define MAX_TIKTOKS 10
 
-static const char *from_file;
-static int from_line;
-static const char *from_func;
+struct tiktok {
+	LARGE_INTEGER hr_timer;
 
-void tik_debug(const char *file, int line, const char *func)
+	const char *from_file;
+	int from_line;
+	const char *from_func;
+};
+
+static struct tiktok tiktoks[MAX_TIKTOKS];
+
+void tik_debug(int n, const char *file, int line, const char *func)
 {
-	hr_timer = KeQueryPerformanceCounter(NULL);
+	if (n < 0 || n >= MAX_TIKTOKS) {
+		printk("Warning: n (%d) out of range\n", n);
+		return;
+	}
+	tiktoks[n].hr_timer = KeQueryPerformanceCounter(NULL);
 
-	from_file = file;
-	from_line = line;
-	from_func = func;
+	tiktoks[n].from_file = file;
+	tiktoks[n].from_line = line;
+	tiktoks[n].from_func = func;
 }
 
-void tok_debug(const char *file, int line, const char *func)
+void tok_debug(int n, const char *file, int line, const char *func)
 {
 	LARGE_INTEGER hr_timer2, hr_freq;
 
+	if (n < 0 || n >= MAX_TIKTOKS) {
+		printk("Warning: n (%d) out of range\n", n);
+		return;
+	}
+
 	hr_timer2 = KeQueryPerformanceCounter(&hr_freq);
-	printk("%s:%d (%s) until %s:%d (%s) took %llu ticks (1/%llu th seconds)\n", from_file, from_line, from_func, file, line, func, hr_timer2.QuadPart - hr_timer.QuadPart, hr_freq.QuadPart);
+	printk("TIKTOK channel %d %s:%d (%s) until %s:%d (%s) took %llu ticks (1/%llu th seconds)\n", n, tiktoks[n].from_file, tiktoks[n].from_line, tiktoks[n].from_func, file, line, func, hr_timer2.QuadPart - tiktoks[n].hr_timer.QuadPart, hr_freq.QuadPart);
 }
