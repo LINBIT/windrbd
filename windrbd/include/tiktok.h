fe6295ee597a2d18a367e469ca4bed94e7d546c0
Johannes Thoma <johannes@johannesthoma.com>
2020-12-03 14:17:13 +0100

    Conditional tiktok
    So we can leave the tiktok instrumentation in the code and
    still turn it off.

--- windrbd/include/tiktok.h
+++ windrbd/include/tiktok.h
@@ -1,10 +1,19 @@
 #ifndef _TIKTOK_H
 #define _TIKTOK_H
 
+/* #define TIKTOK */
+#ifndef TIKTOK
+
+#define tik(n) do { } while (0);
+#define tok(n) do { } while (0);
+
+#else
+
 void tik_debug(int n, const char *file, int line, const char *func);
 void tok_debug(int n, const char *file, int line, const char *func);
 
 #define tik(n) do { tik_debug((n), __FILE__, __LINE__, __func__); } while (0);
 #define tok(n) do { tok_debug((n), __FILE__, __LINE__, __func__); } while (0);
 
+#endif
 #endif
