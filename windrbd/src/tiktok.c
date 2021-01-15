b931ce5eea98ea69d9a549e7088af8194062dcf8
Johannes Thoma <johannes@johannesthoma.com>
2021-01-15 13:59:16 +0100

    tiktok: added description field.

--- windrbd/src/tiktok.c
+++ windrbd/src/tiktok.c
@@ -12,11 +12,12 @@ struct tiktok {
 	const char *from_file;
 	int from_line;
 	const char *from_func;
+	const char *desc;
 };
 
 static struct tiktok tiktoks[MAX_TIKTOKS];
 
-void tik_debug(int n, const char *file, int line, const char *func)
+void tik_debug(int n, const char *desc, const char *file, int line, const char *func)
 {
 	if (n < 0 || n >= MAX_TIKTOKS) {
 		printk("Warning: n (%d) out of range\n", n);
@@ -24,6 +25,7 @@ void tik_debug(int n, const char *file, int line, const char *func)
 	}
 	tiktoks[n].hr_timer = KeQueryPerformanceCounter(NULL);
 
+	tiktoks[n].desc = desc;
 	tiktoks[n].from_file = file;
 	tiktoks[n].from_line = line;
 	tiktoks[n].from_func = func;
@@ -32,7 +34,7 @@ void tik_debug(int n, const char *file, int line, const char *func)
 		tiktoks[n].start_timer = tiktoks[n].hr_timer;
 	tiktoks[n].n++;
 
-	printk("TIKTOK channel %d (#%d) started at %s:%d (%s) 0 ticks\n", n, tiktoks[n].n, file, line, func);
+	printk("TIKTOK channel %d \"%s\" (#%d) started at %s:%d (%s) 0 ticks\n", n, tiktoks[n].desc, tiktoks[n].n, file, line, func);
 }
 
 void tok_debug(int n, const char *file, int line, const char *func)
@@ -52,6 +54,6 @@ void tok_debug(int n, const char *file, int line, const char *func)
 
 	percentage = 100*tiktoks[n].timer_sum.QuadPart/total_runtime.QuadPart;
 
-	printk("TIKTOK channel %d (#%d) %s:%d (%s) until %s:%d (%s) took %llu ticks (1/%llu th seconds)\n", n, tiktoks[n].n, tiktoks[n].from_file, tiktoks[n].from_line, tiktoks[n].from_func, file, line, func, hr_timer2.QuadPart - tiktoks[n].hr_timer.QuadPart, hr_freq.QuadPart);
-	printk("TIKTOK channel %d (#%d) %s:%d (%s) percentage is %.02f total runtime is %llu ticks time spent in region %llu ticks.\n", n, tiktoks[n].n, file, line, func, percentage, total_runtime.QuadPart, tiktoks[n].timer_sum.QuadPart);
+	printk("TIKTOK channel %d \"%s\" (#%d) %s:%d (%s) until %s:%d (%s) took %llu ticks (1/%llu th seconds)\n", n, tiktoks[n].desc, tiktoks[n].n, tiktoks[n].from_file, tiktoks[n].from_line, tiktoks[n].from_func, file, line, func, hr_timer2.QuadPart - tiktoks[n].hr_timer.QuadPart, hr_freq.QuadPart);
+	printk("TIKTOK channel %d \"%s\" (#%d) %s:%d (%s) percentage is %.02f total runtime is %llu ticks time spent in region %llu ticks.\n", n, tiktoks[n].desc, tiktoks[n].n, file, line, func, percentage, total_runtime.QuadPart, tiktoks[n].timer_sum.QuadPart);
 }
