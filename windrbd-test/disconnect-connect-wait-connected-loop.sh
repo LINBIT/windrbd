6e3abfdf079ca8228645d24c45842437761ccc64
Johannes Thoma <johannes@johannesthoma.com>
2021-02-10 19:56:29 +0100

    Working on tests

--- windrbd-test/disconnect-connect-wait-connected-loop.sh
+++ windrbd-test/disconnect-connect-wait-connected-loop.sh
@@ -7,19 +7,19 @@ do
 	i=$[ $i+1 ]
 	echo Disconnect $i
 	drbdadm disconnect $RES
-	drbdadm status
+	drbdadm status $RES
 	sleep 10
-	drbdadm status
+	drbdadm status $RES
 #	echo Disconnect \#2 $i
 #	drbdadm disconnect $RES
 #	drbdadm status
 #	sleep 10
 	echo Connect $i
 	drbdadm connect $RES
-	drbdadm status
+	drbdadm status $RES
 	drbdadm wait-connect $RES
-	drbdadm status
+	drbdadm status $RES
 	echo Connected $i
-	sleep 3
-	drbdadm status
+	sleep 2
+	drbdadm status $RES
 done
