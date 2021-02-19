25eb53ddc43b7cd77361e94d1d66054c74a5bdc4
Johannes Thoma <johannes@johannesthoma.com>
2021-02-19 19:16:22 +0100

    A test an TODO update

---
+++ windrbd-test/disconnect-connect-loop-bsod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+RES=${RES:-windows-windows}
+i=0
+while true
+do
+	i=$[ $i+1 ]
+	date
+	echo Disconnect $RES $i
+	drbdadm disconnect $RES
+	drbdadm status
+	sleep 3
+	drbdadm status
+# TODO: not needed any more?
+	drbdadm attach $RES
+	drbdadm status
+	echo Connect $RES $i
+	drbdadm connect $RES
+	drbdadm status
+	drbdadm wait-connect $RES
+	drbdadm status
+	sleep 3
+	drbdadm status
+done
