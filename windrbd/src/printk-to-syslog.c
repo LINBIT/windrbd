97d1d3aa108b8597a533ff3ab4e545e4ce3c2ca6
Johannes Thoma <johannes@johannesthoma.com>
2021-04-08 19:10:10 +0200

    no net printk
    Until we fix the kref debug loop.

--- windrbd/src/printk-to-syslog.c
+++ windrbd/src/printk-to-syslog.c
@@ -30,7 +30,7 @@
 
 static int no_windows_printk = 0;
 static int no_memory_printk = 0;
-static int no_net_printk = 0;
+static int no_net_printk = 1;
 
 /* TODO: use (and test) O_NONBLOCK sending again, once weird printk
  * losses are fixed.
