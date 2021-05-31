0cc2b3cb2e86994c46f9ad42066d6ea50e683c0c
Johannes Thoma <johannes@johannesthoma.com>
2021-05-31 20:15:11 +0200

    Some more verifier BSODs fixed.
    There seem to be more.

--- windrbd/src/windrbd_device.c
+++ windrbd/src/windrbd_device.c
@@ -1992,12 +1992,19 @@ dbg("Pnp: Is a IRP_MN_QUERY_DEVICE_RELATIONS: s->Parameters.QueryDeviceRelations
 				printk("Warning: number of DRBD devices changed: old %d != new %d\n", num_devices, n);
 			device_relations->Count = num_devices;
 			irp->IoStatus.Information = (ULONG_PTR)device_relations;
-			irp->IoStatus.Status = STATUS_SUCCESS;
 
-			IoCompleteRequest(irp, IO_NO_INCREMENT);
+			IoSkipCurrentIrpStackLocation(irp);
+			status = IoCallDriver(bus_ext->lower_device, irp);
+			if (status != STATUS_SUCCESS) {
+				printk("IoCallDriver returned %x\n", status);
+			}
+			irp->IoStatus.Status = status;
 			num_pnp_bus_requests--;
-			return STATUS_SUCCESS;
+
+//			IoCompleteRequest(irp, IO_NO_INCREMENT);
+			return status;
 		}
+#if 0
 		case TargetDeviceRelation:
 		{
 			struct _DEVICE_RELATIONS *device_relations;
@@ -2023,6 +2030,7 @@ dbg("Pnp: Is a IRP_MN_QUERY_DEVICE_RELATIONS: s->Parameters.QueryDeviceRelations
 			num_pnp_bus_requests--;
 			return STATUS_SUCCESS;
 		}
+#endif
 		default:
 			status = STATUS_NOT_SUPPORTED;
 		}
