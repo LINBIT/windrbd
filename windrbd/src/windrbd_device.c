529b79bdc1df47720a0cc6cc56e38373e0a7bdf0
Johannes Thoma <johannes@johannesthoma.com>
2021-07-19 19:24:53 +0200

    put_page after bio_free().
    Should not crash and fix the memory leak (? is there one ?)

--- windrbd/src/windrbd_device.c
+++ windrbd/src/windrbd_device.c
@@ -1387,16 +1387,15 @@ static void windrbd_bio_finished(struct bio * bio)
 	}
 // printk("free pages %p ...\n", bio);
 /* Pages are freed in bio_free ... */
-#if 0
-	for (i=0;i<bio->bi_vcnt;i++)
-		put_page(bio->bi_io_vec[i].bv_page);
-#endif
 
 printk("Into IoReleaseRemoveLock ... %p windrbd_finished\n", &bio->bi_bdev->ref->w_remove_lock);
 	IoReleaseRemoveLock(&bio->bi_bdev->ref->w_remove_lock, NULL);
 printk("Out of IoReleaseRemoveLock windrbd_finished\n");
 // printk("into bio_put: atomic_read(&bio->bi_cnt) is %d\n", atomic_read(&bio->bi_cnt));
 	bio_put(bio);
+
+	for (i=0;i<bio->bi_vcnt;i++)
+		put_page(bio->bi_io_vec[i].bv_page);
 }
 
 struct io_request {
