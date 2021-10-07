2dacf6d3dde2e79ac4c940701c795ffbe2486639
Johannes Thoma <johannes@johannesthoma.com>
2021-10-07 18:34:25 +0200

    Properly end driver-init task.
    It leaked on driver unload.

--- windrbd/src/disp.c
+++ windrbd/src/disp.c
@@ -217,6 +217,8 @@ DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
 	windrbd_init_boot_device();
 	printk("Start boot device stage1 returned\n");
 
+	return_to_windows(current);
+
 	return STATUS_SUCCESS;
 }
 
