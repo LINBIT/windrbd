54fce8ca4c69249e4c03b44bca6189db18a07cfe
Johannes Thoma <johannes@johannesthoma.com>
2021-10-14 14:39:22 +0200

    Use inf file from Program Files/WinDRBD directory.

--- inno-setup/install-windrbd.cmd
+++ inno-setup/install-windrbd.cmd
@@ -19,4 +19,4 @@ rem This currently does not work. Fix it and reenable this:
 rem windrbd install-bus-device .\windrbd.inf
 
 rem start /wait InfDefaultInstall .\windrbd.inf
-start /wait rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 132 C:\windows\inf\windrbd.inf
+start /wait rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 132 .\windrbd.inf
