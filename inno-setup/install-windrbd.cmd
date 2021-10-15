8ebc8f880b71e7acafd3061661b90187990c20f1
Johannes Thoma <johannes@johannesthoma.com>
2021-10-15 16:34:31 +0200

    New way to install INF file.
    advapi: this only installs it to the system folder (not in
    the driver store). On install-bus-device driver is restored
    from the driver store. Use windrbd -f di-install-driver <inf-file>
    to install driver to the driver store. Hopefully this works now.

--- inno-setup/install-windrbd.cmd
+++ inno-setup/install-windrbd.cmd
@@ -20,6 +20,6 @@ rem windrbd install-bus-device .\windrbd.inf
 
 rem start /wait InfDefaultInstall .\windrbd.inf
 rem start /wait rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 132 .\windrbd.inf
-start /wait rundll32.exe advpack.dll,LaunchINFSectionEx .\windrbd.inf,,,4
+start /wait rundll32.exe advpack.dll,LaunchINFSectionEx .\windrbd.inf,,,20,N
 rem start /wait rundll32.exe advpack.dll,LaunchINFSectionEx .\<file>.inf,,,20
 rem start /wait rundll32.exe advpack.dll,LaunchINFSection .\<file>.inf,DefaultInstall,1
