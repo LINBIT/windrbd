95ee7b964334bba40b5590abe0cca3fcafcd6097
Johannes Thoma <johannes@johannesthoma.com>
2022-02-19 15:37:05 +0100

    Removed WinDRBDDisk hardware ID.
    This one is unknown to Windows.`

--- windrbd/src/windrbd_device.c
+++ windrbd/src/windrbd_device.c
@@ -2337,8 +2337,7 @@ Red_Hat___________VirtIO0
 GenDisk
 */
 				case BusQueryHardwareIDs:
-					len = swprintf(string, L"WinDRBDDisk");
-					len += swprintf(&string[len+1], L"SCSI\\DiskLinbit____________WinDRBD0001")+1;
+					len = swprintf(&string[len+1], L"SCSI\\DiskLinbit____________WinDRBD0001");
 					len += swprintf(&string[len+1], L"SCSI\\DiskLinbit____________WinDRBD")+1;
 					len += swprintf(&string[len+1], L"SCSI\\DiskLinbit__")+1;
 					len += swprintf(&string[len+1], L"SCSI\\Linbit____________WinDRBD0")+1;
