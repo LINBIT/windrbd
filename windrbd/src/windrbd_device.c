dedeb35408974077f5d0f2c71e8167eb817e18ba
Johannes Thoma <johannes@johannesthoma.com>
2022-03-09 20:37:06 +0100

    StorageDeviceAttributesProperty?

--- windrbd/src/windrbd_device.c
+++ windrbd/src/windrbd_device.c
@@ -755,6 +755,9 @@ dbg("ioctl is %x\n", s->Parameters.DeviceIoControl.IoControlCode);
         irp->IoStatus.Information = (ULONG_PTR)CopySize;
         status = STATUS_SUCCESS;
       }
+      if (StoragePropertyQuery->PropertyId == StorageDeviceAttributesProperty && StoragePropertyQuery->QueryType == PropertyStandardQuery) {
+printk("StorageDeviceAttributesProperty ...\n");
+      }
       if (status == STATUS_INVALID_PARAMETER) {
 printk("Invalid IOCTL_STORAGE_QUERY_PROPERTY (PropertyId: %08x / QueryType: %08x)!!\n", StoragePropertyQuery->PropertyId, StoragePropertyQuery->QueryType);
       }
