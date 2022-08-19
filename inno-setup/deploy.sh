a26bf8e0fa5f3aef8f885d912a7f21ce1465358a
Johannes Thoma <johannes@johannesthoma.com>
2022-08-19 15:57:20 +0200

    deploy.sh script from LINSTOR for Windows.

---
+++ inno-setup/deploy.sh
@@ -0,0 +1,13 @@
+if [ $# -lt 2 ]
+then
+    echo "Usage: $0 <filename> <list-of-hosts...>"
+    echo "For example:"
+    echo "$0 install-linstor-server-1.18.2-97-216edb5e6.exe 10.43.224.35 10.43.224.45 10.43.224.48 10.43.224.49 10.43.224.32"
+    exit 1
+fi
+
+FILE=$1
+shift
+
+parallel scp ./$FILE Administrator@{}: ::: $*
+parallel ssh Administrator@{} "./$FILE /verysilent /norestart" ::: $*
