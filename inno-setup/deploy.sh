6fa96e921294684127ba2cbe97a8286894beab58
Johannes Thoma <johannes@johannesthoma.com>
2022-11-14 15:22:23 +0100

    deploy script: new IP addresses of all our Windows VMs

--- inno-setup/deploy.sh
+++ inno-setup/deploy.sh
@@ -2,7 +2,7 @@ if [ $# -lt 2 ]
 then
     echo "Usage: $0 <filename> <list-of-hosts...>"
     echo "For example:"
-    echo "$0 install-linstor-server-1.18.2-97-216edb5e6.exe 10.43.224.35 10.43.224.45 10.43.224.48 10.43.224.49 10.43.224.32"
+    echo "$0 install-windrbd-1.1.3.exe 10.43.224.37 10.43.224.50 10.43.224.69 10.43.224.72 10.43.224.73 10.43.224.74 10.43.224.75"
     exit 1
 fi
 
