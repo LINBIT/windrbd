9ec9ab289dad7ee83a5847b9928805f49707f553
Johannes Thoma <johannes@johannesthoma.com>
2023-08-09 19:04:01 +0000

    Loads into ReactOS but then core dumps.
    Probably somewhere inside init_event_log().

--- windrbd/src/windrbd_device.c
+++ windrbd/src/windrbd_device.c
@@ -2661,6 +2661,8 @@ exit:
 
 static NTSTATUS windrbd_pnp(struct _DEVICE_OBJECT *device, struct _IRP *irp)
 {
+	return STATUS_NOT_IMPLEMENTED;
+#if 0
 	NTSTATUS status;
 
 	if (device == mvolRootDeviceObject || device == user_device_object) {
@@ -3279,6 +3281,7 @@ if (status == STATUS_NOT_SUPPORTED) {
 
 	num_pnp_requests--;
 	return status;
+#endif
 }
 
 static NTSTATUS windrbd_power(struct _DEVICE_OBJECT *device, struct _IRP *irp)
