6d5fb732cd93f6b979f6058150cc34b0c21cebba
Johannes Thoma <johannes@johannesthoma.com>
2023-08-14 13:26:34 +0000

    Imported headers from ReactOS
    This probably has to be repeated over time.

---
+++ windrbd/include/from-reactos/psdk/warning.h
@@ -0,0 +1,5 @@
+
+/* FIXME - For WDK compatibility.
+   Here #pragma warning are placed and are disabled or enabled.
+   GCC and MSVC use different pragmas so a compatible list for
+   these remains to be found. */
