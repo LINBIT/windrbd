ad051a9d096ae712e72ffaab701c9daec45ad0c4
Johannes Thoma <johannes@johannesthoma.com>
2023-09-15 12:43:03 +0000

    Working on shell script removing old WinDRBD versions from the driver store.

--- misc/del-all-linbit-drivers.sh
+++ misc/del-all-linbit-drivers.sh
@@ -1,1 +1,5 @@
-for i in $( pnputil -e | grep LINBIT -B 5 | grep oem | cut -c 29-36 ) ; do pnputil -d $i ; done
+# have to adjust -c param ... TODO: this should be
+# done by the installer
+# for i in $( pnputil -e | grep LINBIT -B 5 | grep oem | cut -c 29-37 ) ; do pnputil -d $i ; done
+# something like this:
+for i in $( pnputil -e | grep provider.*Linbit -B 1 | grep oem | cut -c 29-37 | tr -d ' ' ) ; do echo pnputil -d $i ; done
