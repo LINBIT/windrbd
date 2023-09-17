89f13566adfa5fe38a68247068a9974956a5edac
Johannes Thoma <johannes@johannesthoma.com>
2023-09-17 20:26:04 +0000

    ReactOS: install driver.
    Just copy it, create a service and start it ...

--- windrbd/include/windrbd_config.h
+++ windrbd/include/windrbd_config.h
@@ -1,9 +1,11 @@
 #ifndef _WINDRBD_CONFIG_H
 #define _WINDRBD_CONFIG_H
 
-/* Windows (not ReactOS) target, with mingw */
-/* Enable this for networking support: */
-// #define CONFIG_HAVE_NETIO_DRIVER 1
+#ifndef REACTOS
+#define CONFIG_HAVE_NETIO_DRIVER 1
+#endif
+/* Else Windows (not ReactOS) target, with mingw */
+
 // #define CONFIG_HAVE_IO_CREATE_DEVICE_SECURE 1
 // #define CONFIG_HAVE_RW_LOCKS 1
 // #define CONFIG_HAVE_NO_EXECUTE 1
