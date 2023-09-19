2a9fd9cde7a1a355f82b8461fadb6fe557930916
Johannes Thoma <johannes@johannesthoma.com>
2023-09-19 17:33:00 +0000

    windrbd config changes.
    Never 'don't set AddDevice' - will BSOD.

--- windrbd/include/windrbd_config.h
+++ windrbd/include/windrbd_config.h
@@ -1,12 +1,12 @@
 #ifndef _WINDRBD_CONFIG_H
 #define _WINDRBD_CONFIG_H
 
+/* Windows (not ReactOS) target, with mingw */
 #ifndef REACTOS
 #define CONFIG_HAVE_NETIO_DRIVER 1
-#define CONFIG_DONT_HAVE_BUS_DEVICE 1
 #endif
-/* Else Windows (not ReactOS) target, with mingw */
 
+/* Those need to be implemented: */
 // #define CONFIG_HAVE_IO_CREATE_DEVICE_SECURE 1
 // #define CONFIG_HAVE_RW_LOCKS 1
 // #define CONFIG_HAVE_NO_EXECUTE 1
