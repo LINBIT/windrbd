78e3f8a13ff146ea753749645ba68e262f6b59c9
Johannes Thoma <johannes@johannesthoma.com>
2023-11-09 17:29:44 +0000

    Document about how to set up busy write resync stall test.

--- windrbd-test/busy-write-one-block.sh
+++ windrbd-test/busy-write-one-block.sh
@@ -1,1 +1,12 @@
+# secondary:
+drbdadm disconnect busy-resync-test
+drbdadm invalidate busy-resync-test
+# primary
+# ./fio-3.27-x64/fio --name=r:\\fio-test --size=1000m --rw=write --direct=1 --thread --runtime=600
+# (done my invalidate now)
+# primary
 ./fio-3.27-x64/fio --name=r:\\fio-test --size=4k --rw=write --direct=1 --bs=4k --iodepth=32 --thread --runtime=600 --time_based --numjobs=32
+# secondary:
+drbdadm connect busy-resync-test
+# secondary:
+drbdsetup events2
