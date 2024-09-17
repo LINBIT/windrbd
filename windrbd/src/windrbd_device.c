7654783237797ed0e648d875dad7cd39a64be88e
Johannes Thoma <johannes@johannesthoma.com>
2024-09-17 13:45:51 +0000

    No extra ObDereferenceObject
    This causes a BSOD later.

--- windrbd/src/windrbd_device.c
+++ windrbd/src/windrbd_device.c
@@ -2891,8 +2891,10 @@ printk("IRP_MN_REMOVE_DEVICE 6a irql is %p\n", KeGetCurrentIrql());
 printk("Extra IoReleaseRemoveLock for ReactOS ...\n");
 IoReleaseRemoveLock(&bdev->ref->w_remove_lock, NULL);
 #endif
+/*
 printk("Extra ObDereferenceObject ...\n");
 ObDereferenceObject(device);
+*/
 
 printk("IRP_MN_REMOVE_DEVICE 7 bdev is %p\n", bdev);
 					}
