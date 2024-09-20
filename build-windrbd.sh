4b2eed116b0dffe9a787a4461e39dd2e044e8271
Johannes Thoma <johannes@johannesthoma.com>
2024-09-20 15:45:40 +0000

    Name on nexus is now install-windrbd-latest-from-gitlab.exe
    So we can hardcode the Version number later ...
    build script now works as expected.

--- build-windrbd.sh
+++ build-windrbd.sh
@@ -44,7 +44,7 @@ ssh johannes@10.43.224.39 "cd /tmp/build-windrbd
 for i in inno-setup/install-windrbd-*.exe
 do
 	echo copiing \$i to nexus ...
-	curl -f --netrc-file /etc/nexus-password --upload-file \$i https://nexus.at.linbit.com/repository/windows/WinDRBD/\$i
+	curl -f --netrc-file /etc/nexus-password --upload-file \$i https://nexus.at.linbit.com/repository/windows/WinDRBD/install-windrbd-latest-from-gitlab.exe
 done
 "
 
