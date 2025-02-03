ea0d4f82d3250203db7fcb719c56120787caaf56
Johannes Thoma <johannes@johannesthoma.com>
2025-02-03 12:56:34 +0000

    Enabled net printk's again.

--- windrbd/src/printk-to-syslog.c
+++ windrbd/src/printk-to-syslog.c
@@ -43,7 +43,7 @@
 static int no_event_log_printk = 0;
 static int no_windows_printk = 0;
 static int no_memory_printk = 0;
-static int no_net_printk = 1;
+static int no_net_printk = 0;
 /* Use it at your own risk! (need to check IRQL) */
 static int no_serial_printk = 1;
 
