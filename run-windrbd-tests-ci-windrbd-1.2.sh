31014b6cecabde68f367d03181c054fd720573a0
Johannes Thoma <johannes@johannesthoma.com>
2025-03-05 17:32:59 +0000

    Only run invalid-names

--- run-windrbd-tests-ci-windrbd-1.2.sh
+++ run-windrbd-tests-ci-windrbd-1.2.sh
@@ -18,10 +18,10 @@ export LINBIT_CI_MAX_CPUS=3
 # This is too much for my Linux VM:
 # export LINBIT_CI_MAX_CPUS=5
 
-time ./virter/run-test.sh --variant=windows
+# time ./virter/run-test.sh --variant=windows
+time ./virter/run-test.sh --variant=windows --torun=invalid-names
 
 # time ./virter/run-test.sh --base-image=windows-server-2019-created-2023 --startvm=250 --torun=add-connect-delete,add-path-multiple-times,connect,diskless,double-promote-diskless,invalid-names,outdate,quorum,quorum-failover-reconnect,rename,resync-after,resync-after-failover-to-dless,resync-initial,split-brain,stress-connect-and-2pc,suspend-io
 # time ./virter/run-test.sh --base-image=windows-server-2019-created-2023 --startvm=250 --torun=add-connect-delete,add-path-multiple-times,connect,diskless,double-promote-diskless,invalid-names,outdate,quorum,quorum-failover-reconnect,rename,resync-after,resync-after-failover-to-dless,resync-initial,split-brain,stress-connect-and-2pc,suspend-io --repeats=3
-# time ./virter/run-test.sh --base-image=windows-server-2019-created-2023 --startvm=250 --torun=invalid-names
 # all tests 3 times.
 # time ./virter/run-test.sh --base-image=windows-server-2019-created-2023 --startvm=250 --repeats=3
