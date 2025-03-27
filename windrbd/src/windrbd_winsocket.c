916310031139edc032c8bb2eb08fca792ed3b70e
Johannes Thoma <johannes@johannesthoma.com>
2025-03-27 16:39:45 +0000

    Revert "Enabled dump_packets again."
    This reverts commit fd4834cdb98f790f76c20882b631c97ef554196b.

--- windrbd/src/windrbd_winsocket.c
+++ windrbd/src/windrbd_winsocket.c
@@ -1331,11 +1331,9 @@ static int wsk_recvmsg(struct socket *socket, struct msghdr *msg, struct kvec *v
 /* Do nothing..just for debugging ... */
 static void dump_packet(unsigned char *buf, size_t buflen)
 {
-/*
 	return;
-*/
 
-#if 1
+#if 0
 	size_t i;
 	char s[80];
 	int pos;
