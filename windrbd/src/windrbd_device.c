0c9ea0036e6776c9628d341fe18e24774f0159e9
Johannes Thoma <johannes@johannesthoma.com>
2025-04-25 14:02:42 +0000

    Some printk's
    We (at least) need to implement SCSIOPs:
    
    SCSIOP_INQUIRY (for reporting vendor, ...)
    SCSIOP_SYNCHRONIZE_CACHE (for sending a flush to WinDRBD)

--- windrbd/src/windrbd_device.c
+++ windrbd/src/windrbd_device.c
@@ -2938,8 +2938,11 @@ static NTSTATUS __attribute__((stdcall)) windrbd_scsi(struct _DEVICE_OBJECT *dev
 	}
 	status = STATUS_SUCCESS;	/* optimistic */
 
+printk("srb->Function is 0x%08x\n", srb->Function);
+
 	switch (srb->Function) {
 	case SRB_FUNCTION_EXECUTE_SCSI:
+printk("cdb->AsByte[0] is 0x%02x\n", cdb->AsByte[0]);
 		switch (cdb->AsByte[0]) {
 		case SCSIOP_TEST_UNIT_READY:
 			srb->SrbStatus = SRB_STATUS_SUCCESS;
@@ -3227,6 +3230,7 @@ static NTSTATUS __attribute__((stdcall)) windrbd_scsi(struct _DEVICE_OBJECT *dev
 			break;
 		}
 
+// TODO: SCSIOP_INQUIRY
 		default:
 			printk("SCSI OP %x not supported\n", cdb->AsByte[0]);
 			status = STATUS_NOT_IMPLEMENTED;
