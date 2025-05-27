2a27f4103b84f03ac15e7a1c0d0c7f207aa06027
Johannes Thoma <johannes@johannesthoma.com>
2025-05-27 12:55:21 +0000

    gitlab CI: run diskless test 5 times.

--- run-windrbd-tests-ci-windrbd-1.2.sh
+++ run-windrbd-tests-ci-windrbd-1.2.sh
@@ -26,7 +26,7 @@ export VIRTER_TIME_SSH_PING_COUNT=600
 
 # time ./virter/run-test.sh --variant=windows
 # time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=20
-time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=3
+time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=5
 # time ./virter/run-test.sh --variant=windows --torun=invalid-names
 
 # time ./virter/run-test.sh --base-image=windows-server-2019-created-2023 --startvm=250 --torun=add-connect-delete,add-path-multiple-times,connect,diskless,double-promote-diskless,invalid-names,outdate,quorum,quorum-failover-reconnect,rename,resync-after,resync-after-failover-to-dless,resync-initial,split-brain,stress-connect-and-2pc,suspend-io
