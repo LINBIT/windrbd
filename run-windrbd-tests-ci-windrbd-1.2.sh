6246cf60b1deda83f4c40873808c9daaeb71b51f
Johannes Thoma <johannes@johannesthoma.com>
2025-06-17 16:06:25 +0000

    CI: All windows tests 3 times

--- run-windrbd-tests-ci-windrbd-1.2.sh
+++ run-windrbd-tests-ci-windrbd-1.2.sh
@@ -30,9 +30,9 @@ export VIRTER_TIME_SSH_PING_COUNT=600
 export VIRTER_TIME_SHUTDOWN_TIMEOUT=300s
 
 # time ./virter/run-test.sh --variant=windows
-# time ./virter/run-test.sh --variant=windows --repeats=5
+time ./virter/run-test.sh --variant=windows --repeats=3
 # time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=20
-time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=100
+# time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=100
 # time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=5
 # time ./virter/run-test.sh --variant=windows --torun=invalid-names
 
