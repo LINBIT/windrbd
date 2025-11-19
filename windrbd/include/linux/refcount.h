39cb918670d7856bfcf7a4434958ff9cdea1f765
Johannes Thoma <johannes@johannesthoma.com>
2025-11-19 17:36:31 +0000

    Linux compat: refcount_inc and refcount_sub_and_test

--- windrbd/include/linux/refcount.h
+++ windrbd/include/linux/refcount.h
@@ -19,6 +19,23 @@ static inline void refcount_set(refcount_t *r, int n)
 }
 
 /**
+ * refcount_inc - increment a refcount
+ * @r: the refcount to increment
+ *
+ * Similar to atomic_inc(), but will saturate at REFCOUNT_SATURATED and WARN.
+ *
+ * Provides no memory ordering, it is assumed the caller already has a
+ * reference on the object.
+ *
+ * Will WARN if the refcount is 0, as this represents a possible use-after-free
+ * condition.
+ */
+static inline void refcount_inc(refcount_t *r)
+{
+	atomic_inc(&r->refs);
+}
+
+/**
  * refcount_dec_and_test - decrement a refcount and test if it is 0
  * @r: the refcount
  *
@@ -36,6 +53,32 @@ static inline __must_check bool refcount_dec_and_test(refcount_t *r)
 	return atomic_dec_and_test(&r->refs);
 }
 
+
+/**
+ * refcount_sub_and_test - subtract from a refcount and test if it is 0
+ * @i: amount to subtract from the refcount
+ * @r: the refcount
+ *
+ * Similar to atomic_dec_and_test(), but it will WARN, return false and
+ * ultimately leak on underflow and will fail to decrement when saturated
+ * at REFCOUNT_SATURATED.
+ *
+ * Provides release memory ordering, such that prior loads and stores are done
+ * before, and provides an acquire ordering on success such that free()
+ * must come after.
+ *
+ * Use of this function is not recommended for the normal reference counting
+ * use case in which references are taken and released one at a time.  In these
+ * cases, refcount_dec(), or one of its variants, should instead be used to
+ * decrement a reference count.
+ *
+ * Return: true if the resulting refcount is 0, false otherwise
+ */
+static inline __must_check bool refcount_sub_and_test(int i, refcount_t *r)
+{
+	return atomic_sub_and_test(i, &r->refs);
+}
+
 /**
  * refcount_read - get a refcount's value
  * @r: the refcount
@@ -46,5 +89,6 @@ static inline unsigned int refcount_read(const refcount_t *r)
 {
 	return atomic_read(&r->refs);
 }
+
 
 #endif
