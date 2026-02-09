a7130e1aca0598e5da759ba0b77920d53c1a44fb
Johannes Thoma <johannes@johannesthoma.com>
2026-02-09 11:57:46 +0000

    3 times windows tests, 3 times cross_platform tests.

--- run-windrbd-tests-ci-test-this-branch.sh
+++ run-windrbd-tests-ci-test-this-branch.sh
@@ -33,9 +33,9 @@ export VIRTER_TIME_SSH_PING_COUNT=600
 export VIRTER_TIME_SHUTDOWN_TIMEOUT=300s
 
 # time ./virter/run-test.sh --variant=windows
-# time ./virter/run-test.sh --variant=windows --repeats=3
-# time ./virter/run-test.sh --variant=cross_platform --repeats=3
-time ./virter/run-test.sh --variant=cross_platform
+time ./virter/run-test.sh --variant=windows --repeats=3
+time ./virter/run-test.sh --variant=cross_platform --repeats=3
+# time ./virter/run-test.sh --variant=cross_platform
 # time ./virter/run-test.sh --variant=windows --repeats=10
 # time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=3
 # time ./virter/run-test.sh --variant=windows --torun=diskless --repeats=100
