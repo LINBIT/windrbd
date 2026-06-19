4fee012393d429515f8bb42643246a60d50318b6
Johannes Khoshnazar-Thoma <johannes@johannesthoma.com>
2026-06-19 17:54:41 +0200

    NOT checking boot sector.

--- windrbd/src/windrbd_device.c
+++ windrbd/src/windrbd_device.c
@@ -2288,6 +2288,9 @@ int windrbd_check_for_filesystem_and_maybe_start_faking_partition_table(struct b
 	int err;
 	KIRQL flags;
 
+printk("NOT checking boot sector\n");
+return 0;
+
 		/* Are we primary? If not, do nothing. */
 	if (bdev->drbd_device == NULL ||
             bdev->drbd_device->resource == NULL ||
